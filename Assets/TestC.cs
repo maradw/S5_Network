492301b175712bb14ac906ef27b35d268de513aa
maradw <maria.mendoza@tecsup.edu.pe>
2025-04-14 11:38:35 -0500

    base

---
+++ Assets/TestC.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class TestC : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        print("Conect");
+        PhotonNetwork.NickName =MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion; //
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        print("coneccted");
+        print(PhotonNetwork.LocalPlayer.NickName);
+    }
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("disconnected" + cause.ToString());
+    }
+}
