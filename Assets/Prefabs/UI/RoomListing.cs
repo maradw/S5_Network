af302839b291b7b5ae06cd4cf8d487b12a7c0b55
maradw <maria.mendoza@tecsup.edu.pe>
2025-04-26 19:46:21 -0500

    nose

--- Assets/Prefabs/UI/RoomListing.cs
+++ Assets/Prefabs/UI/RoomListing.cs
@@ -4,18 +4,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using Photon.Pun;
 
 public class RoomListing : MonoBehaviour
 {
     [SerializeField]
     private TextMeshProUGUI _text;
-
+    private RoomInfo _player;
     public RoomInfo _roomInfo { get; private set; }
-    public void SetRoomInfo(RoomInfo roomInfo) 
+    
+    public void SetRoomInfo(RoomInfo roomInfo)
     {
         _roomInfo = roomInfo;
         _text.text = roomInfo.MaxPlayers + "," + roomInfo.Name;
     }
-    
-   
+    public void OnClick_Button()
+    {
+        PhotonNetwork.JoinRoom(RoomInfo.Name);
+    }
+
+
+
 }
